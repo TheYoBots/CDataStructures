71eab9219bea60e5ada24e54f4261c95f0e0a6d5
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2022-05-23 18:15:51 +0530

    Quick Sort Descending

---
+++ Sorting/Quick Sort/program2.c
@@ -0,0 +1,49 @@
+// Quick Sort - Descending
+
+#include<stdio.h>
+
+void QuickSort(int a[], int left, int right);
+
+void main( )
+{
+    int n,i,a[10];
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    QuickSort(a,0,n-1);
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%4d",a[i]);
+    }
+}
+
+void QuickSort(int a[], int left, int right)
+{
+    int pivot,t,i,j,l,r;
+    if(left<right)
+    {
+        pivot=a[left];
+        l=left;
+        r=right;
+        while(l<r)
+        {
+            while(a[l]<=pivot && l<=right)
+                l++;
+            while(a[r]>pivot)
+                r--;
+            if(l<r)
+            {
+                t=a[l];
+                a[l]=a[r];
+                a[r]=t;
+            }
+        }
+        t=a[left];
+        a[left]=a[r];
+        a[r]=t;
+        QuickSort(a, left, r-1);
+        QuickSort(a, r+1, right);
+    }
+}
